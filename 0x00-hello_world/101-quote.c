af616f3fce5e1aecffab8fa7f42be526c7bd1cd9
Khalil Touzani <khalil1010-@hotmail.fr>
2023-06-24 10:53:41 -0700

    0x00. C - Hello, World tasks (updated)

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
-#include<unistd.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
  *
  * Description: print a quote using write function
- * 	ssize_t write(int fd, const void *buf, size_t count);
+ *	ssize_t write(int fd, const void *buf, size_t count);
  *
  * Return: 1 (Error).
 */
-
 
 int main(void)
 {	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
