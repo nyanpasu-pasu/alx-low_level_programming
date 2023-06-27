03beb13bd30a73c284f53ec15a0fac575f25ed13
Khalil Touzani <khalil1010-@hotmail.fr>
2023-06-27 13:02:37 -0700

    7-print_tebahpla.c task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prog Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	cahr c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
